1e73dcbc2d62a8880a612ab4b6ee375761a42963
Felix-Musila <felixmusilakioko@gmail.com>
2023-02-16 05:24:11 -0800

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /* My first C-code */
-
 int main(void)
 {
 	char string1[] = "Programming is like building a multilingual puzzle";
 
 	puts(string1);
 
-	return 0;
+	return (0);
 }
 
