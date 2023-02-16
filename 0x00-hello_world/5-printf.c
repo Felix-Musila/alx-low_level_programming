e3f5eb35b76c13309fbf3accc7e25709dbcea955
Felix-Musila <felixmusilakioko@gmail.com>
2023-02-16 14:03:12 -0800

    Update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,15 @@
 #include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Result: 0 Always Success
+ *
+ */
+int main(void);
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n);
+
+	return (0);
+}
+
