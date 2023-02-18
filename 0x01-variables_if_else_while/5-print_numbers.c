bea7499969bee61e1d4404ae7fcaf9b5f42f05fa
Felix-Musila <felixmusilakioko@gmail.com>
2023-02-18 00:08:43 +0000

    Update

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  * main - Program that prints 0-9
- * Return 0 Success
+ * Return: 0 Success
  */
 int main(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
-		printf("%d" , i);
+		printf("%d", i);
 	putchar('\n');
 	return (0);
 }
