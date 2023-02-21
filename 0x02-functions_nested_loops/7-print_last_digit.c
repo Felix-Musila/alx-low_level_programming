44614fa22107597a4a8d4b1dd5b63dcc4958cc8b
Felix-Musila <felixmusilakioko@gmail.com>
2023-02-21 20:46:34 +0000

    ...

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,12 @@ int print_last_digit(int i)
 {
 	if (i <= 9)
 	{
-		_putchar(i);		
+		_putchar('i');		
 		return(i);
 	}
 	else
 	{
-		_putchar(i % 10);
+		_putchar('i' % 10);
 		return(i % 10);
 	}
 }
