9d9b7aeb1122de220c3fd5fb4ddcedb921d90634
Felix-Musila <felixmusilakioko@gmail.com>
2023-02-21 19:58:42 +0000

    ...

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - A function that computes the absolute value of i
+ * @i: The integer to be checked
+ * Return: 0 Success
+ */
+
+int _abs(int)
+{
+	int i;
+
+	if (i >= 0)
+		_putchar(i + '0');
+	else
+		_putchar((i * -1) + '0');
+	return (0);
+}
