bc0721b20ad1c88042b41858ead73af5d4ebbdc3
Felix-Musila <felixmusilakioko@gmail.com>
2023-02-22 12:13:46 +0000

    ...

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,26 +7,21 @@
 
 void times_table(void)
 {
-	int i;
 	int j;
 	int k;
 
-	for (i = 0; i <= 9; i++)
+	for (j = 0; j <= 9; j++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (k = 0; k <= 9; k++)
 		{
-			for (k = 0; k <= 9; k++)
+			_putchar((k * j) + '0');
+			if (k == 9)
+				_putchar('\n');
+			else
 			{
-				_putchar((k * j) + '0');
-				
-				if (k == 9)
-					_putchar('\n');
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 			}
 		}
 	}
