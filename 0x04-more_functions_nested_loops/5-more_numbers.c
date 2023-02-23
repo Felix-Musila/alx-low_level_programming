e08e1f97556e6491f1c77de510cf59e528a007ea
Felix-Musila <felixmusilakioko@gmail.com>
2023-02-23 19:54:59 +0000

    ...

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,18 +10,15 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 1; i < 10; i++)
+	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			char c;
 			int tens = ((j % 10) + 10);
 
-			for (c = 48; c <= 57; c++)
-			{
-				_putchar(c);
-			}
-			if (j > 9)
+			if (j < 9)
+				_putchar(j + '0');
+			else
 				_putchar(tens + '0');
 		}
 		_putchar('\n');
