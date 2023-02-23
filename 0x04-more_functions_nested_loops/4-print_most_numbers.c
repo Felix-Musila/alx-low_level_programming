fb224bb157cf91592290d5b09ce537669152e3ae
Felix-Musila <felixmusilakioko@gmail.com>
2023-02-23 15:39:42 +0000

    ...

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,17 +7,12 @@
 
 void print_most_numbers(void)
 {
-	int i = 0;
-
-	while (i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
 		if (i == 2 || i == 4)
 			continue;
 		else
-		{
-			_putchar(i + '0');
-			i++;
-		}
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
